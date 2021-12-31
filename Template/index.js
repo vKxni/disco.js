3bf3b041cbefd8fb677f6e5c30b1cd3634ee27db
Koni <74613145+vKxni@users.noreply.github.com>
2021-12-31 14:17:13 +0100

    added template

---
+++ Template/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+require("./src/util/bot.js");
+
+require("./src/util/db.js");
---
+++ Template/src/commands/Utility/ping.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { CommandInteraction, Permissions } = require("discord.js");
+
+module.exports.cooldown = {
+    length: 10000, /* in ms */
+    users: new Set()
+};
+
+/**
+ * Runs ping command.
+ * @param {CommandInteraction} interaction The Command Interaciton
+ * @param {any} utils Additional util
+ */
+module.exports.run = async (interaction, utils) =>
+{
+    try
+    {
+        
+
+        await interaction.reply({ content: `Pong!`, ephemeral: true });
+        return;
+    }
+    catch (err)
+    {
+        return Promise.reject(err);
+    }
+};
+
+module.exports.permissions = {
+    clientPermissions: [Permissions.FLAGS.SEND_MESSAGES],
+    userPermissions: [Permissions.FLAGS.SEND_MESSAGES]
+};
+
+module.exports.data = new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("View the Ping of the Bot");
